bd10ab744fbfb50b257361cd1080e995e25caf9c
CarolinaFuenzalida <carolinapazfuenzalidapoblete@gmail.com>
2022-07-04 14:56:59 -0400

    cambios de proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';//
 
-console.log(cipher);
+// console.log(cipher); //
+
+function cifrarTexto() {
+  var mensaje = document.getElementById("cajaTexto").value;
+  document.getElementById("demo").innerHTML = mensaje;
+}
