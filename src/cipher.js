52e66fe339852e1c0e28c27125bb511626a3e625
CarolinaFuenzalida <carolinapazfuenzalidapoblete@gmail.com>
2022-07-14 02:40:57 -0400

    proyecto final

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,48 @@
-const cipher = {}
-
+const cipher = {
+  encode: function (msje, offset) {
+    const array = msje.toString().split(""); // el array se divide
+    var resultado = ""; // variable donde se guardará el string que dé como resultado
+    for (var i = 0; i < array.length; i++) {
+      var numAscii = array[i].charCodeAt(); // se asigna a cada caracter su valor ascii
+      if (numAscii >= 65 && numAscii <= 90) { //verifica que las letras ingresadas sean mayusculas
+        let posicion = ((numAscii - 65 + offset * 1) % 26) + 65; // se suma el numero ascii a el valor en cual se quiere encriptar el mensaje
+        array[i] = String.fromCharCode(posicion); // se trae de vuelta el numero ascii a caracter
+        resultado += array[i]; // se juntan todos los valores devueltos en el array
+      } else if (numAscii >= 97 && numAscii <= 122) { //verifica que las letras ingresadas sean minusculas
+        let posicion = ((numAscii - 97 + offset * 1) % 26) + 97;
+        array[i] = String.fromCharCode(posicion);
+        resultado += array[i];
+      } else if (numAscii == 32) { //verifica que el valor ingresado sea un espacio
+        array[i] = String.fromCharCode(32);
+        resultado += array[i];
+      } else {
+        resultado += array[i];
+        //alert("Porfavor ingresa letras Mayúsculas, Minúsculas o espacios");
+      }
+    }
+    return resultado; // se devuelve el resultado a index.js para mostrar como resultado
+  },
+  decode: function (msje, offset) { // lo mismo pero al reves 
+    const array = msje.toString().split("");
+    var resultado = "";
+    for (var i = 0; i < array.length; i++) {
+      var numAscii = array[i].charCodeAt();
+      if (numAscii >= 65 && numAscii <= 90) {
+        let posicion = ((numAscii + 65 - offset * 1) % 26) + 65;
+        array[i] = String.fromCharCode(posicion);
+        resultado += array[i];
+      } else if (numAscii >= 97 && numAscii <= 122) {
+        let posicion = (numAscii + 122 - (offset * 1) % 26) - 122;
+        array[i] = String.fromCharCode(posicion);
+        resultado += array[i];
+      } else if (numAscii == 32) {
+        array[i] = String.fromCharCode(32);
+        resultado += array[i];
+      } else {
+        alert("Porfavor ingresa letras Mayúsculas, Minúsculas o espacios");
+      }
+    }
+    return resultado;
+  },
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,31 @@
-// import cipher from './cipher.js';
+import cipher from "./cipher.js"
+
+  // var abecedario =  ???
+  let botonCifrar = document.getElementById("cifrar"); // botón para llamar a encode
+  let botonDescifrar = document.getElementById("descifrar"); //botón para llamar a decode
+  let textoCifrado = document.getElementById("resultado");  // resultado que se mostrara como respuesta
+
+  
+  botonCifrar.addEventListener('click', mostrarCifrado) // click llama a accion a la funcion encode
+
+function mostrarCifrado(){ // funcion que envia los datos puesto por el usuario a cipher.js
+  let msje = document.getElementById("texto").value;
+ if (msje === '') {   
+   alert  ("Debes ingresar texto");
+ }
+  let offset = document.getElementById("rango").value; //dato de input 
+  let cifradoMsje = cipher.encode(msje,offset); //datos enviados
+  textoCifrado.value = cifradoMsje; // datos recibidos son igual a respuesta
+}
+
+botonDescifrar.addEventListener('click', mostrarDescifrado)
+
+function mostrarDescifrado(){
+  let msje = document.getElementById("texto").value;
+  let offset = document.getElementById("rango").value;
+  let cifradoMsje = cipher.decode(msje,offset);
+  textoCifrado.value = cifradoMsje;
+}
+  
 
 //console.log(cipher); 
-
-function cipher() {
-  var msj = document.getElementById("texto").value; // tomar el texto dado por usuario
-  var encrypt = document.getElementById("rango").value; // tomar el rango
-  const array = msj.split(""); // crear array en donde se guardara texto y se divide letra por letra
-  var resultado = ""; // crea variable de resultado vacia y al final se le agrega el valor del texto ya cifrado
-  for (var i = 0; i < array.length; i++) { 
-      var numAscii = array[i].charCodeAt(); // crea variable en donde se asigna el n ascii de cada letra
-      let posicion = parseInt(numAscii += (encrypt * 1)); // se compara el n ascii del texto con el ingresado a rango, 
-      //se deja *1 ya que el rango ingresa como string, con el *1 se convierte a number
-      if (posicion > 90) { //compara que la nueva posicion sumada arriba, llegue hasta 90 que es la Z
-        //si se cumple sigue hacia abajo, en donde 
-          posicion = 65 + (encrypt * 1) - 1; //le puse el menos 1 pq se corria uno demas 
-      }
-      array[i] = String.fromCharCode(posicion); //luego lo convertimos a string desde su posicion en n ascii
-      resultado += array[i]; //se guarda cada resultado formando el array y se dicta que resultado es igual al array 
-      //guardado luego de que pasa por la funcion
-  }
-
-  document.getElementById("resultado").innerHTML = resultado;
-};
-
-function descifrar() {
-  var msj = document.getElementById("texto").value;
-  var encrypt = document.getElementById("rango").value;
-  msj = msj.toUpperCase();
-  const array = msj.split("");
-  var resultado = "";
-  for (var i = 0; i < array.length; i++) {
-      var numAscii = array[i].charCodeAt();
-      let posicion = parseInt(numAscii -= (encrypt * 1));
-      if (posicion < 65) { // lo mismo de arriba pero todo al reves
-          posicion = 90 - (encrypt * 1) + 1; // lo mismo de arriba pero se suma 
-      }
-      array[i] = String.fromCharCode(posicion);
-      resultado += array[i];
-  }
-
-
-  document.getElementById("resultado").innerHTML = resultado;
-};
